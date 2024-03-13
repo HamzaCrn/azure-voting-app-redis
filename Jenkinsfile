7a2f4cbcdfb71c88136b30f6a7e1feb7490e781a
Hamza Crnovrsanin <hcrnovrsanin@barracuda.com>
2024-03-13 16:41:39 +0100

    commit changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
    stages {
 stage('Run Tests') {
          steps {
-            bat(script: 'pytest ./tests/test_sample.py')
+            bat(script: 'python ./tests/test_sample.py')
          }
          post {
             success {
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,10 +1,7 @@
-// Define the function to increment a number
-function inc(x) {
-    return x + 1;
-}
+def inc(x):
+    return x + 1
 
-// Define a test using Jest
-test('increment function increments the input by 1', () => {
-    expect(inc(4)).toBe(5);
-});
+
+def test_answer():
+    assert inc(4) == 5
 
